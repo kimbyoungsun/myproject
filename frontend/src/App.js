d4cc6031acba0ca71c17adf569963bcc7c8aad22
kimbyoungsun <sloth0317.work@gmail.com>
2025-03-10 15:29:39 +0900

    add : myproject

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/hello/")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>React와 Django 연결 테스트</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
